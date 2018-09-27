0a8809fbccae81e5af3326dff69dcf774a01f391
Tichomir Mitkov <MitkovTihomir@gmail.com>
2018-09-27 15:53:11 +0300

    Actually drawing

--- canvas.tcl
+++ canvas.tcl
@@ -1,8 +1,38 @@
-#!/usr/bin/tclsh
+#!/usr/bin/env wish
 
-package require Tk;
+namespace eval canvas {
 
-proc cscroll {
+	set height 0
+	set width 0
+	set bar 0
 
+	proc configure { width_ height_} {
+		variable width
+		variable height	
+		variable bar
 
+		set width $width_
+		set height $height_
+
+		puts [.c coords $bar]
+		after 10 ".c coords $bar 40 0 41 $height"
+	}
+
+	proc draw {} {
+		variable height
+		variable width 
+		variable bar
+
+		set bar [.c create rectangle 40 0 40 50 -fill black]
+		puts "barId: $bar"
+	}
 }
+
+proc main {} {
+	pack [canvas .c] -fill both -expand yes
+	bind .c <Configure> { canvas::configure %w %h }
+
+	canvas::draw
+}
+
+main
