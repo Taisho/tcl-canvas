e6107167f93100c3893324ded6d6d9e0b300ea0b
Tichomir Mitkov <MitkovTihomir@gmail.com>
2018-09-27 18:00:29 +0300

    In this version the code is broken. Trying to wrap the canvas in a megawidget

--- canvas.tcl
+++ canvas.tcl
@@ -1,38 +1,50 @@
 #!/usr/bin/env wish
 
-namespace eval canvas {
+namespace eval CSlider {
 
-	set height 0
-	set width 0
-	set bar 0
+	proc new { name } {
+		set bar 0
 
-	proc configure { width_ height_} {
-		variable width
-		variable height	
-		variable bar
+		canvas "$name"
 
-		set width $width_
-		set height $height_
-
-		puts [.c coords $bar]
-		after 10 ".c coords $bar 40 0 41 $height"
-	}
-
-	proc draw {} {
-		variable height
-		variable width 
-		variable bar
-
-		set bar [.c create rectangle 40 0 40 50 -fill black]
+		set bar [$name create rectangle 40 0 40 50 -fill black]
 		puts "barId: $bar"
+
+		rename ::$name ::mega$name
+
+		set script [concat { proc } ::$name { { args } {
+				set option [lindex $args 0]
+
+				if {[string compare $option configure] == 0 } {
+					set width [lindex $args 1]
+					set height [lindex $args 2]
+					} $name {{coords $bar 40 0 41 50}
+					after 1 
+				}	
+			}
+
+		}]
+
+		puts $name
+		eval $script
+		return $name
 	}
+
+proc ::w {} {
+	puts WWWWWWWWWWWWWWWW
+}
+}
+
+proc cslider { name } {
+	CSlider::new $name	
+	puts "name: $name"
+	return $name
 }
 
 proc main {} {
-	pack [canvas .c] -fill both -expand yes
-	bind .c <Configure> { canvas::configure %w %h }
-
-	canvas::draw
+	pack [cslider .c] -fill both -expand yes
+	.c configure 40 50
+	#bind .c <Configure> { ::%W configure %w %h }
 }
 
 main
